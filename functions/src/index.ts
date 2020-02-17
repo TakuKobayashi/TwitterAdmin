9485684484921502140dfee2753bdd9f85785c04
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-02-17 12:41:21 +0900

    expressでtwitter apiを呼び出せるような形にした

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,16 @@
-import * as functions from 'firebase-functions';
+import { config, region } from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+require('dotenv').config();
+admin.initializeApp(config().firebase);
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' });
+});
+
+export const api = region('asia-northeast1').https.onRequest(app);
+
+export default app;
