c71ded9709b8f0fdde2106f6c8b088a4e614e763
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-02-19 12:52:06 +0900

    ダッシュボードにTwitterのタイムラインを表示

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import SEO from '../components/SEO';
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
 
 const Home = () => {
   return (
     <div>
       <SEO title="Home" keywords={['OAH', 'application', 'react']} />
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="taptappun"
+        options={{height: 400}}
+      />
     </div>
   );
 };
