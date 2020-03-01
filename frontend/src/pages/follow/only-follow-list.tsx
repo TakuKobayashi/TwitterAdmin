fd1914ca162daf0962dbe7c6e50dafa986f6bf0a
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2020-03-01 12:44:02 +0900

    Userの情報を表示してチェックボックスで制御できるようにした

--- frontend/src/pages/follow/only-follow-list.tsx
+++ frontend/src/pages/follow/only-follow-list.tsx
@@ -1,11 +1,18 @@
-import { Row, Col, Card, List, ListItem, User } from 'oah-ui';
+import { Row, Col, Card, List, ListItem, User, Checkbox } from 'oah-ui';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import SEO from '../../components/SEO';
 
 const OnlyFollowListPage = () => {
+  const onChangeCheckbox = (isChecked: boolean, user: any) => {
+    console.log(isChecked)
+    console.log(user)
+    // value will be true or false
+  };
+
   const [followers, setFollowers] = useState(0);
   useEffect(() => {
+    /*
     const requestFunc = async () => {
       const response = await axios.get(process.env.API_ROOT_URL + '/twitter/followers/only_follows', {
         headers: {
@@ -17,6 +24,7 @@ const OnlyFollowListPage = () => {
       console.log(response.data);
     };
     requestFunc();
+    */
   }, []);
   const userList = [
     { name: 'Carla Espinosa', title: 'Nurse' },
@@ -29,13 +37,15 @@ const OnlyFollowListPage = () => {
     <>
       <SEO title="List" keywords={['OAH', 'application', 'react']} />
       <Row>
-        <Col breakPoint={{ xs: 12, md: 6 }}>
-          <Card size="Small">
+        <Col breakPoint={{ xs: 12, md: 12 }}>
+          <Card size="Large">
             <header>Users List</header>
             <List>
               {userList.map((user, index) => (
                 <ListItem key={index}>
-                  <User title={user.title} name={user.name} />
+                  <Checkbox status="Success" onChange={(isChecked: boolean) => onChangeCheckbox(isChecked, user)}>
+                  <User title={user.title} name={user.name} image={"url(https://pbs.twimg.com/profile_images/940479838854377473/_Exr3_lx_200x200.jpg)"} />
+                  </Checkbox>
                 </ListItem>
               ))}
             </List>
